42fc9391502d53e725876b4c1b11843eb6e76f68
Akiko Okabe <aokabe@developers.wyyerd.com>
2018-04-09 16:36:23 -0600

    update initial set up

---
+++ src/components/LogIn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>Log In</h2>
+      <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  data () {
+    return {
+      msg: 'Welcome to Esquire Embroidery!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
