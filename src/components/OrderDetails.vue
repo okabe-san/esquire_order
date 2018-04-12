5d58fadd2670ffced6d31048b8be4cb5ce8e1804
Akiko Okabe <aokabe@developers.wyyerd.com>
2018-04-12 14:19:30 -0600

    add upload file field

--- src/components/OrderDetails.vue
+++ src/components/OrderDetails.vue
@@ -14,6 +14,7 @@
       <input type="checkbox" v-model="re_order">
       {{re_order}}
 
+      <!-- for re-order -->
       <div v-if="re_order == true">
         <!-- check exact same order or not -->
         Is this the exact same order?
@@ -30,7 +31,29 @@
             </li>
           </ul>
         </div>
+      </div>
 
+      <!-- for non re-order -->
+      <div v-else>
+        <!-- upload -->
+        <form enctype="multipart/form-data" v-if="isInitial || isSaving">
+          <h3>Upload embroidery files</h3>
+          <div class="dropbox">
+            <input type="file"
+              multiple
+              :name="uploadFieldName"
+              :disabled="isSaving"
+              @change="filesChange($event.target.name, $event.target.files);
+              fileCount = $event.target.files.length"
+              class="input-file">
+              <p v-if="isInitial">
+                Drag your file(s) here to begin<br /> or click to browse
+              </p>
+              <p v-if="isSaving">
+                Uploading {{ fileCount }} files...
+              </p>
+          </div>
+        </form>
       </div>
 
       <!-- nav buttons -->
@@ -40,12 +63,25 @@
 </template>
 
 <script>
+// for file upload
+const STATUS_INITIAL = 0
+const STATUS_SAVING = 1
+const STATUS_SUCCESS = 2
+const STATUS_FAILED = 3
+
 export default {
   name: 'Detail',
   data () {
     return {
-      orders: []
+      orders: [],
+      uploadedFiles: [],
+      uploadError: null,
+      currentStatus: null,
+      uploadFieldName: ''
     }
+  },
+  mounted () {
+    this.reset()
   },
   created () {
     this.loadOrders()
@@ -76,6 +112,19 @@ export default {
       set (value) {
         this.$store.dispatch('updateOrderPicked', value)
       }
+    },
+    // for file upload
+    isInitial () {
+      return this.currentStatus === STATUS_INITIAL
+    },
+    isSaving () {
+      return this.currentStatus === STATUS_SAVING
+    },
+    isSuccess () {
+      return this.currentStatus === STATUS_SUCCESS
+    },
+    isFailed () {
+      return this.currentStatus === STATUS_FAILED
     }
   },
   methods: {
@@ -88,6 +137,35 @@ export default {
     },
     next () {
       this.$router.push('/summary')
+    },
+    // for file upload
+    reset () {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL
+      this.uploadedFiles = []
+      this.uploadError = null
+    },
+    save (formData) {
+      // upload data to the server
+      this.currentStatus = STATUS_SAVING
+
+      // FIXME: develop upload file(s)
+    },
+    filesChange (fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData()
+
+      if (!fileList.length) return
+
+      // append the files to FormData
+      Array
+        .from(Array(fileList.length).keys())
+        .map(x => {
+          formData.append(fieldName, fileList[x], fileList[x].name)
+        })
+
+      // save it
+      this.save(formData)
     }
   }
 }
@@ -112,4 +190,32 @@ li {
 .step {
   color: orange;
 }
+.dropbox {
+    outline: 2px dashed grey; /* the dash box */
+    outline-offset: -10px;
+    background: lightcyan;
+    color: dimgray;
+    padding: 10px 10px;
+    min-height: 200px; /* minimum height */
+    position: relative;
+    cursor: pointer;
+  }
+
+  .input-file {
+    opacity: 0; /* invisible but it's there! */
+    width: 100%;
+    height: 200px;
+    position: absolute;
+    cursor: pointer;
+  }
+
+  .dropbox:hover {
+    background: lightblue; /* when mouse over to the drop zone, change color */
+  }
+
+  .dropbox p {
+    font-size: 1.2em;
+    text-align: center;
+    padding: 50px 0;
+  }
 </style>
