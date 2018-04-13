663d25860649229460f76eef0e42092373f61c45
Akiko Okabe <aokabe@developers.wyyerd.com>
2018-04-13 15:48:00 -0600

    add back button to summary page

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -9,7 +9,11 @@
     <main>
       <h1>Review</h1>
       <!-- step 1 -->
-      <h3>Step 1</h3>
+      <h3>
+        Step 1
+        <button @click="backToShipping">Edit</button>
+      </h3>
+
       Representative Name: {{state.rep}} <br />
       Garments from: {{state.garment}} <br />
       Shipping method: {{state.method}} <br />
@@ -24,13 +28,21 @@
      </ul>
 
      <!-- step 2 -->
-     <h3>Step 2</h3>
+     <h3>
+       Step 2
+        <button @click="backToDetails">Edit</button>
+      </h3>
+
      Re-order: {{state.re_order}} <br />
      Order {{state.order_picked}} <br />
      Need edit: {{state.edit_order}} <br />
 
      <!-- step 3 -->
-     <h3>Step 3</h3>
+     <h3>
+       Step 3
+       <button @click="backToOptions">Edit</button>
+     </h3>
+
      <ul>
       <li v-for="(option, index) in state.option_checked" :key="index">
         {{index + 1}} {{option}}
@@ -51,6 +63,17 @@ export default {
       get () {
         return this.$store.state
       }
+    }
+  },
+  methods: {
+    backToShipping () {
+      this.$router.push('/shipping')
+    },
+    backToDetails () {
+      this.$router.push('/details')
+    },
+    backToOptions () {
+      this.$router.push('/options')
     }
   }
 }
