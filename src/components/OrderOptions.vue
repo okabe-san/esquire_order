4cea749c427f4f666cace5d1ebb265361aeb7144
Akiko Okabe <aokabe@developers.wyyerd.com>
2018-05-08 11:37:12 -0600

    add checkbox style

--- src/components/OrderOptions.vue
+++ src/components/OrderOptions.vue
@@ -1,72 +1,88 @@
 <template>
-  <div>
+  <div id="order">
     <section>
-      <h3 class="step">Step 1</h3>
-      <h3 class="step">Step 2</h3>
-      <h3 class="step">Step 3</h3>
+      <h3>Step 1</h3>
+      <i class="material-icons step">keyboard_arrow_down</i>
+      <h3>
+        Step 2<br />
+      </h3>
+      <i class="material-icons step">keyboard_arrow_down</i>
+      <h2 class="step">
+        Step 3<br />
+        <small>Option</small>
+      </h2>
+      <i class="material-icons step">keyboard_arrow_down</i>
       <h3>Review</h3>
     </section>
+
     <main>
-      <h1>Options</h1>
-      <!-- options list -->
-      Select option(s)
-      <ul>
-        <li v-for="option in options" :key="option">
-          {{option}}
-          <input type="checkbox" v-model="optionChecked" :value="option">
+      <div class="form_wrapper">
+        <div class="form">
+          <h2>Options</h2>
 
-          <!-- FOR OPTION 2 -->
-          <div v-if="
-            optionChecked.includes(option) &&
-            option === 'Poly Bagged by name'">
-            <h3>Upload list</h3>
-            <!-- upload file -->
-            <vue-clip ref="vc" :options="optionDetails" :on-added-file="fileAddedBagged">
-              <template slot="clip-uploader-action" slot-scope="props">
-                <div class="uploader-action" :class="{dragging: props.dragging}">
-                  <div class="dz-message">
-                    Drag and Drop file
-                  </div>
+          <!-- options list -->
+          <ul>
+            <li v-for="option in options" :key="option">
+              {{option}}&nbsp;
+              <label class="container">
+                <input type="checkbox" v-model="optionChecked" :value="option">
+                <span class="checkbox"></span>
+              </label>
+
+              <!-- FOR OPTION 2 -->
+              <div v-if="
+                optionChecked.includes(option) &&
+                option === 'Poly Bagged by name'">
+                <h3>Upload list</h3>
+                <!-- upload file -->
+                <vue-clip ref="vc" :options="optionDetails" :on-added-file="fileAddedBagged">
+                  <template slot="clip-uploader-action" slot-scope="props">
+                    <div class="uploader-action" :class="{dragging: props.dragging}">
+                      <div class="dz-message">
+                        Drag and Drop file
+                      </div>
+                    </div>
+                  </template>
+                </vue-clip>
+                <div v-for="(file, index) in baggedByName" :key="index">
+                  {{file.name}}
+                  <button @click="removeFileAddedBagged(index)">Delete</button>
                 </div>
-              </template>
-            </vue-clip>
-            <div v-for="(file, index) in baggedByName" :key="index">
-              {{file.name}}
-              <button @click="removeFileAddedBagged(index)">Delete</button>
-            </div>
-          </div>
+              </div>
 
-          <!-- FOR OPTION 5 -->
-          <div v-if="
-            optionChecked.includes(option) &&
-            option === 'Personalization'">
-            <h3>Upload list</h3>
-            <!-- upload file -->
-            <vue-clip ref="vc" :options="optionDetails" :on-added-file="fileAddedPersonal">
-              <template slot="clip-uploader-action" slot-scope="props">
-                <div class="uploader-action" :class="{dragging: props.dragging}">
-                  <div class="dz-message">
-                    Drag and Drop file
-                  </div>
+              <!-- FOR OPTION 5 -->
+              <div v-if="
+                optionChecked.includes(option) &&
+                option === 'Personalization'">
+                <h3>Upload list</h3>
+                <!-- upload file -->
+                <vue-clip ref="vc" :options="optionDetails" :on-added-file="fileAddedPersonal">
+                  <template slot="clip-uploader-action" slot-scope="props">
+                    <div class="uploader-action" :class="{dragging: props.dragging}">
+                      <div class="dz-message">
+                        Drag and Drop file
+                      </div>
+                    </div>
+                  </template>
+                </vue-clip>
+                <div v-for="(file, index) in personalizationName" :key="index">
+                  {{file.name}}
+                  <button @click="removeFileAddedPersonal(index)">Delete</button>
                 </div>
-              </template>
-            </vue-clip>
-            <div v-for="(file, index) in personalizationName" :key="index">
-              {{file.name}}
-              <button @click="removeFileAddedPersonal(index)">Delete</button>
-            </div>
-          </div>
-        </li>
-      </ul>
+              </div>
+            </li>
+          </ul>
 
-      <!-- comments -->
-      Additional Comments
-      <input type="text" v-model="comments">
-      <br />
+          <!-- comments -->
+          Additional Comments
+          <input type="text" v-model="comments">
+          <br />
 
-      <!-- nav buttons -->
-      <button @click="back">Back</button>
-      <button @click="next">Next</button>
+          <!-- nav buttons -->
+          <button @click="back">Back</button>
+          <button @click="next">Next</button>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -136,22 +152,15 @@ export default {
 </script>
 
 <style scoped>
-main {
-  box-sizing: border-box;
-  float: right;
-  width: 80%;
+@import '../assets/css/order_lib.css';
+@import '../assets/css/button_lib.css';
+/* for steps */
+h2 {
+  font-weight: 700;
+  margin: 0;
+}
+section > h3 {
+  color: rgba(255, 255, 255, 0.5);
+}
 
-  padding-right: 10%;
-}
-section {
-  box-sizing: border-box;
-  float: left;
-  width: 20%;
-}
-li {
-  list-style-type: none;
-}
-.step {
-  color: orange;
-}
 </style>
