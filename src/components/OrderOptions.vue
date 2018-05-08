b9b47f61bb21dd121afc967f47db7dbfb8b2ebc5
Akiko Okabe <aokabe@developers.wyyerd.com>
2018-05-08 11:48:43 -0600

    move option adding file to component

--- src/components/OrderOptions.vue
+++ src/components/OrderOptions.vue
@@ -28,59 +28,19 @@
                 <input type="checkbox" v-model="optionChecked" :value="option">
                 <span class="checkbox"></span>
               </label>
-
-              <!-- FOR OPTION 2 -->
-              <div v-if="
-                optionChecked.includes(option) &&
-                option === 'Poly Bagged by name'">
-                <h3>Upload list</h3>
-                <!-- upload file -->
-                <vue-clip ref="vc" :options="optionDetails" :on-added-file="fileAddedBagged">
-                  <template slot="clip-uploader-action" slot-scope="props">
-                    <div class="uploader-action" :class="{dragging: props.dragging}">
-                      <div class="dz-message">
-                        Drag and Drop file
-                      </div>
-                    </div>
-                  </template>
-                </vue-clip>
-                <div v-for="(file, index) in baggedByName" :key="index">
-                  {{file.name}}
-                  <button @click="removeFileAddedBagged(index)">Delete</button>
-                </div>
-              </div>
-
-              <!-- FOR OPTION 5 -->
-              <div v-if="
-                optionChecked.includes(option) &&
-                option === 'Personalization'">
-                <h3>Upload list</h3>
-                <!-- upload file -->
-                <vue-clip ref="vc" :options="optionDetails" :on-added-file="fileAddedPersonal">
-                  <template slot="clip-uploader-action" slot-scope="props">
-                    <div class="uploader-action" :class="{dragging: props.dragging}">
-                      <div class="dz-message">
-                        Drag and Drop file
-                      </div>
-                    </div>
-                  </template>
-                </vue-clip>
-                <div v-for="(file, index) in personalizationName" :key="index">
-                  {{file.name}}
-                  <button @click="removeFileAddedPersonal(index)">Delete</button>
-                </div>
-              </div>
             </li>
           </ul>
 
           <!-- comments -->
           Additional Comments
-          <input type="text" v-model="comments">
-          <br />
+          <textarea cols="40" rows="3" v-model="comments">
+          </textarea>
 
           <!-- nav buttons -->
-          <button @click="back">Back</button>
-          <button @click="next">Next</button>
+          <div class="action">
+            <button @click="back">Back</button>
+            <button @click="next">Next</button>
+          </div>
         </div>
       </div>
     </main>
@@ -92,12 +52,7 @@ export default {
   name: 'Options',
   data () {
     return {
-      options: [],
-      optionDetails: {
-        url: '/details'
-      },
-      baggedByName: [],
-      personalizationName: []
+      options: []
     }
   },
   created () {
@@ -161,6 +116,9 @@ h2 {
 }
 section > h3 {
   color: rgba(255, 255, 255, 0.5);
+}
+.action {
+  margin-top: 3rem;
 }
 
 </style>
